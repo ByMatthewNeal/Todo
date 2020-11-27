aed6ba4a23f858e52a6bc9347712c1577136449c
ByMatthewNeal <mneal.jw@gmail.com>
2020-11-27 14:04:02 -0500

    Added styling for the deleted item to drop off screen

--- app.js
+++ app.js
@@ -47,7 +47,11 @@
     // DELETE TODO
      if(item.classList[0] === 'trash-btn') {
          const todo = item.parentElement;
-         todo.remove();
+         // Animation
+         todo.classList.add('fall');
+         todo.addEventListener('transitioned', function (){
+             todo.remove()
+         })
      }
 
      //CHECK MARK 
